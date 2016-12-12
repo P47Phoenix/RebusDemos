3342e2f42d48d25cdc0162216e2f6f670fbe6102
Mogens Heller Grabe <mookid8000@gmail.com>
2016-12-12 10:12:02 +0100

    removed stuff

--- Demo0/Server/Program.cs
+++ Demo0/Server/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using Rebus.Activation;
-using Rebus.Config;
-using Rebus.Logging;
-
+﻿
 #pragma warning disable 1998
 
 namespace Server
@@ -11,21 +7,6 @@ namespace Server
     {
         static void Main()
         {
-            using (var activator = new BuiltinHandlerActivator())
-            {
-                activator.Handle<string>(async message =>
-                {
-                    Console.WriteLine($"Received message {message}");
-                });
-
-                Configure.With(activator)
-                    .Logging(l => l.ColoredConsole(LogLevel.Warn))
-                    .Transport(t => t.UseMsmq("server"))
-                    .Start();
-
-                Console.WriteLine("Press ENTER to quit");
-                Console.ReadLine();
-            }
         }
     }
 }
